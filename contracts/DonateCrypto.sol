eb863532219177c1c4f2dc415ea2c4c0ba0a5606
Orban Szilard <orbanszlrd@gmail.com>
2022-09-21 13:00:48 +0200

    Create contract

--- contracts/DonateCrypto.sol
+++ contracts/DonateCrypto.sol
@@ -1,4 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract DonateCrypto {}
+error NotOwner();
+
+contract DonateCrypto {
+    address public immutable i_owner;
+
+    address[] public donors;
+
+    mapping(address => uint256) public addressToAmount;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    function donate() public payable {
+        addressToAmount[msg.sender] += msg.value;
+        donors.push(msg.sender);
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 i = 0; i < donors.length; i++) {
+            address funder = donors[i];
+            addressToAmount[funder] = 0;
+        }
+        donors = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }('');
+        require(callSuccess, 'Call failed');
+    }
+
+    fallback() external payable {
+        donate();
+    }
+
+    receive() external payable {
+        donate();
+    }
+}
